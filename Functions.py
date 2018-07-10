d42083b64e94cadc0a24a4403dede503dad0107b
Moses Cliff <mozescliff@gmail.com>
2018-07-10 16:52:32 +0300

    Set the data type in the input function instead of in the print function

--- Functions.py
+++ Functions.py
@@ -1,34 +1,47 @@
 # defining the Function, Minutes here is the input, hours is the variable, the function returns the variable
 input_mins = input("Please eater the number of minutes: ")
 
+
 def minutes_to_hours(minutes):
     hours = minutes / 60
     return hours
 
-print("In",int(input_mins),"There are",minutes_to_hours(int(input_mins)),"Hours")
 
-#This function here returns the number of seconds in that hour from the function above
+print("In", int(input_mins), "There are",
+      minutes_to_hours(int(input_mins)), "Hours")
+
+# This function here returns the number of seconds in that hour from the function above
+
+
 def hours_to_seconds(hours):
     seconds = hours * 60 * 60
     return seconds
+
 
 b = minutes_to_hours(int(input_mins))
 
 print(hours_to_seconds(b))
 
-#Convert celcious to string
+# Convert celcious to string
+
+
 def celcious_to_farenheight(degrees):
-    farenheight = degrees *(9/5) + 32
+    farenheight = degrees * (9/5) + 32
     return farenheight
 
-degrees = input("Please enter the temp: ")
 
-print("The temp in Farenheight is",celcious_to_farenheight(int(degrees)))
+degrees = int(input("Please enter the temp: "))
 
-#length of string function
+print("The temp in Farenheight is", celcious_to_farenheight(degrees))
+
+# length of string function
+
+
 def length(word):
     wordlength = len(word)
     return wordlength
+
+
 word = input("Please enter the word: ")
 
-print("The length of th word is",length(word))
+print("The length of th word is", length(word))
--- advancedfunctions.py
+++ advancedfunctions.py
@@ -1,10 +1,10 @@
 #Advanced function {Functions with Multiple parameters}
-input_mins = input("Please eater the number of minutes: ")
-input_seconds = input("Please enter the number of seconds: ")
+input_mins = int(input("Please eater the number of minutes: "))
+input_seconds = int(input("Please enter the number of seconds: "))
 
 def minutes_to_hours(minutes,seconds):
     hours = minutes / 60 + seconds / 3600
     return hours
 
 #When you call the function, make sure all the parameters are passed
-print(minutes_to_hours(int(input_mins),int(input_seconds)))
+print(minutes_to_hours(input_mins,input_seconds))
