94f40c5c8e627dc7aaf1b960fe92b50f512a7bd0
Moses Cliff <mozescliff@gmail.com>
2018-07-10 15:44:40 +0300

    Added advaned functions

--- Functions.py
+++ Functions.py
@@ -1,16 +1,19 @@
 # defining the Function, Minutes here is the input, hours is the variable, the function returns the variable
-a = input("Please eater the number of minutes: ")
+input_mins = input("Please eater the number of minutes: ")
+
 def minutes_to_hours(minutes):
     hours = minutes / 60
     return hours
 
-print("In",int(a),"There are",minutes_to_hours(int(a)),"Hours")
+print("In",int(input_mins),"There are",minutes_to_hours(int(input_mins)),"Hours")
 
 #This function here returns the number of seconds in that hour from the function above
 def hours_to_seconds(hours):
     seconds = hours * 60 * 60
     return seconds
 
-b = minutes_to_hours(int(a))
+b = minutes_to_hours(int(input_mins))
 
 print(hours_to_seconds(b))
+
+
