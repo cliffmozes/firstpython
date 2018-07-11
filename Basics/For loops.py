d7bde3ea0c8d041a99210d8cdf9620a93be91b83
Moses Cliff <mozescliff@gmail.com>
2018-07-11 12:43:06 +0300

    Added advanced loops examples

--- Basics/For loops.py
+++ Basics/For loops.py
@@ -1,9 +1,9 @@
 # for loops
 # array
-names = ["moses@gmail.com", "Cliff@gmail.com ", "Mwanga@hotmail.com"]
+fnames = ["moses@gmail.com", "Cliff@gmail.com ", "Mwanga@hotmail.com"]
 
 # name is a variable for each item in the array
-for name in names:
+for name in fnames:
 
     if "gmail" in name:
      print(name)
@@ -19,3 +19,49 @@ def mylist(lower,upper):
 c =[an,bn]    
 for n in c:
     print(n)
+# 2. loop in in a function with user input
+temp1=int(input("Enter Temp1: "))
+temp2=int(input("Enter Temp2: "))
+temp3=int(input("Enter Temp3: "))
+def celcious_to_farenheight(degrees):
+    farenheight = degrees * (9/5) + 32
+    return farenheight
+
+c=[temp1,temp2,temp3]
+for tem in c:
+    if celcious_to_farenheight(tem) < -275:
+        
+        print("Temp is too low")
+        
+    else:
+            
+        print(celcious_to_farenheight(tem))
+# looping in multiple lists
+# for loops
+# array
+fnames = ["moses", "Cliff", "Mwanga"]
+snames= ["Hello","This","Is"]
+
+# name is a variable for each item in the array
+for f,s in zip(fnames,snames):
+    
+    #if "gmail" in name:
+     print(f,s)
+    
+#For loop in function
+
+temp=[10,-10,-289,100]
+def celcious_to_farenheight(degrees):
+    farenheight = degrees * (9/5) + 32
+    return farenheight
+
+
+for tem in temp:
+    if celcious_to_farenheight(tem) < -275:
+        
+        print("Temp is too low")
+        
+    else:
+            
+        print(celcious_to_farenheight(tem))
+
--- Basics/tempCodeRunnerFile.py
+++ Basics/tempCodeRunnerFile.py
@@ -1,8 +1,16 @@
-password = ""
+temp1=int(input("Enter Temp1: "))
+temp2=int(input("Enter Temp2: "))
+temp3=int(input("Enter Temp3: "))
+def celcious_to_farenheight(degrees):
+    farenheight = degrees * (9/5) + 32
+    return farenheight
 
-while password != password:
-    password = input("Please enter you password: ")
-    if password == password:
-        print("logged in")
+c=[temp1,temp2,temp3]
+for tem in c:
+    if celcious_to_farenheight(tem) < -275:
+        
+        print("Temp is too low")
+        
     else:
-        print("Wrong Password")
+            
+        print(celcious_to_farenheight(tem))
