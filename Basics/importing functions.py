724eb92b08381790a321f18e633efb827ea0b00f
Moses Cliff <mozescliff@gmail.com>
2018-07-11 13:01:08 +0300

    added file handling

--- Basics/importing functions.py
+++ Basics/importing functions.py
@@ -1,9 +1,8 @@
+from Test import celcious_to_farenheight
 temp1=int(input("Enter Temp1: "))
 temp2=int(input("Enter Temp2: "))
 temp3=int(input("Enter Temp3: "))
-def celcious_to_farenheight(degrees):
-    farenheight = degrees * (9/5) + 32
-    return farenheight
+
 
 c=[temp1,temp2,temp3]
 for tem in c:
